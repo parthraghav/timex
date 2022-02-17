662fc361d85d8ef170da5c7781eabd45601f27ec
Parth Raghav <iniento12@gmail.com>
2022-02-17 17:12:20 +0530

    added trade model

---
+++ src/trade.rs
@@ -0,0 +1,9 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Debug)]
+pub struct Trade {
+	pub bid_order_id: String,
+	pub ask_order_id: String,
+	pub trade_price: f64,
+	pub created_at: DateTime<Utc>,
+}
