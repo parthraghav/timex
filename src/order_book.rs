a4dac7ba037771326a31af47876236f300609c6e
Parth Raghav <iniento12@gmail.com>
2022-02-21 12:46:53 +0530

    cannot add market orders to illliquid books

--- src/order_book.rs
+++ src/order_book.rs
@@ -42,22 +42,39 @@ impl OrderBook {
 
 	pub fn add_order(&mut self, order: Order) {
 		if matches!(order.action, OrderAction::Buy) {
-			self.bid_queue.push(Bid {
-				order: Order::clone(&order),
-			});
-			println!("Buy order successfully added:\n{:?}", order);
+			if self.bid_queue.len() != 0 && !matches!(order.family, OrderFamily::Market) {
+				self.bid_queue.push(Bid {
+					order: Order::clone(&order),
+				});
+				println!("Buy order successfully added:\n{:?}", order);
+			} else {
+				println!(
+					"Cannot add a market order to an illiquid book:\n{:?}",
+					order
+				);
+			}
 		} else if matches!(order.action, OrderAction::Sell) {
-			self.ask_queue.push(Ask {
-				order: Order::clone(&order),
-			});
-			println!("Sell order successfully added:\n{:?}", order);
+			if self.ask_queue.len() != 0 && !matches!(order.family, OrderFamily::Market) {
+				self.ask_queue.push(Ask {
+					order: Order::clone(&order),
+				});
+				println!("Sell order successfully added:\n{:?}", order);
+			} else {
+				println!(
+					"Cannot add a market order to an illiquid book:\n{:?}",
+					order
+				);
+			}
 		}
 	}
 
-	pub fn match_orders(&mut self) {
+	pub fn print_bids(&mut self) {
 		while let Some(bid) = self.bid_queue.pop() {
 			println!("{:?}", bid.order);
 		}
+	}
+
+	pub fn print_asks(&mut self) {
 		while let Some(ask) = self.ask_queue.pop() {
 			println!("{:?}", ask.order);
 		}
