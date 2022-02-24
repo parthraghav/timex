6e228afbaa6a7910c8fd21c16c49ba35ee601bfd
Parth Raghav <iniento12@gmail.com>
2022-02-24 16:36:56 +0530

    testing orderbook

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use order::*;
 use order_book::*;
 
 fn main() {
-    let mut order_book = OrderBook::new(String::from("DUMMY"), 100000);
+    let mut order_book = OrderBook::new(String::from("DUMMY"), 100000, 10.0);
 
     order_book.add_order(order::Order {
         id: String::from("0x0"),
@@ -24,5 +24,46 @@ fn main() {
         updated_at: chrono::offset::Utc::now(),
     });
 
-    order_book.match_orders();
+    order_book.add_order(order::Order {
+        id: String::from("0x0"),
+        quantity: 2,
+        price: 120.05,
+        family: OrderFamily::Limit,
+        action: OrderAction::Buy,
+        status: OrderStatus::Open,
+        duration: OrderDuration::Day,
+        market_ticker: String::from("dummy_token"),
+        created_at: chrono::offset::Utc::now(),
+        updated_at: chrono::offset::Utc::now(),
+    });
+
+    order_book.add_order(order::Order {
+        id: String::from("0x0"),
+        quantity: 3,
+        price: 90.05,
+        family: OrderFamily::Limit,
+        action: OrderAction::Sell,
+        status: OrderStatus::Open,
+        duration: OrderDuration::Day,
+        market_ticker: String::from("dummy_token"),
+        created_at: chrono::offset::Utc::now(),
+        updated_at: chrono::offset::Utc::now(),
+    });
+
+    order_book.add_order(order::Order {
+        id: String::from("0x0"),
+        quantity: 4,
+        price: 100.001,
+        family: OrderFamily::Limit,
+        action: OrderAction::Sell,
+        status: OrderStatus::Open,
+        duration: OrderDuration::Day,
+        market_ticker: String::from("dummy_token"),
+        created_at: chrono::offset::Utc::now(),
+        updated_at: chrono::offset::Utc::now(),
+    });
+
+    println!("---------------------");
+
+    order_book.settle();
 }
